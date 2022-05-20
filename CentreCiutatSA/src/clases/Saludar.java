08b25e71c5b72e65ab32652be99f1d091f18ca9a
Miriamfernandez <miriamfernandez@estudiante.edib.es>
2022-05-20 20:41:30 +0200

    Commit de Miriam!

--- CentreCiutatSA/src/clases/Saludar.java
+++ CentreCiutatSA/src/clases/Saludar.java
@@ -5,6 +5,9 @@ public class Saludar {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("FURULA?");
+		
+		System.out.println("Hola, soy Miriam!");
+		
 	}
 
 }
