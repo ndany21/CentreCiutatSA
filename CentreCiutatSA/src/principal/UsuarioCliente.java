0c7cdd42addd3ea360df99e08f63bb010e1e4a2e
Tursito <theadritur@gmail.com>
2022-05-27 19:52:25 +0200

    login con metodos extraidos

--- CentreCiutatSA/src/principal/UsuarioCliente.java
+++ CentreCiutatSA/src/principal/UsuarioCliente.java
@@ -61,63 +61,18 @@ public class UsuarioCliente {
 				 
 				 
 				 if (nombreUsuario.equals(nombreIntroducido) && esAdmin.equals("0") ) {
-						//SQL que recoga los datos y poner el nombre del usuario 
-					  	
-					  	System.out.println("Bienvenido " + nombreUsuario + " " + apellidosUsuario);
-					  	System.out.println(" ");
-					  	
-						
-					  	 
-						while(!salir){
-						System.out.println("========== QUE DESEA HACER? ==========");
-						System.out.println(" ");
-					    System.out.println("(Seleccione la opción que desee con ");
-						System.out.println("las teclas * 1 - 2 o 3* )");
-						System.out.println(" ");
-						System.out.println(" 1) Mostrar información de su vehiculo");
-						System.out.println(" 2) Mostrar su información personal");
-						System.out.println(" 3) Salir de la aplicación ");
-						System.out.println(" ");
-						System.out.println("======================================");
-						System.out.println(" ");
-						opcion = sc.nextInt();
-						
-
-						switch(opcion){
-					    	case 1:
-					    	
-					    		//CONTECTAR CON BASE DE DATOS Y MOSTRAR POR PANTALLA INFORMACIÓN DEL VEHÍCULO DEL CLIENTE
-					    		
-					    	break;
-						case 2:
-							
-								//CONTECTAR CON BASE DE DATOS Y MOSTRAR POR PANTALLA INFORMACIÓN DEL CLIENTE 
-							
-							c1.buscartuInformacion(con);
-							
-					    	break;       	
-					 	case 3:                      	 
-					    	salir=true;
-					    	System.out.println("== SALIENDO DEL PROGRAMA ==");
-					    	System.out.println(" ");
-					    	System.out.println("Gracias por tu colaboración " +nombreIntroducido+ ", que tengas un buen dia");
-					    	System.exit(0);
-					    	break;
-					 	default:
-					    	System.out.println("Solo números entre 1 y 3");
-					    	System.out.println(" ");  	 
-					  	 
-					  	}	 
-						} 
+					salir = menuCliente(con, c1, sc, salir, nombreIntroducido, nombreUsuario, apellidosUsuario); 
+					
 
 					}else if(nombreUsuario.equals(nombreIntroducido) && esAdmin.equals("1")){
 					
-						System.out.println("Menu admin");
+						salir = menuAdmin(sc, salir); 	    	 
 					}
 				 
 				 
+				 
 				 else {
-					 
+						System.out.println("No se encuentra en la base de datos  ");
 					 }
 				  	
 				
@@ -140,9 +95,125 @@ public class UsuarioCliente {
 		}
 
 	
-		System.out.println("No se encuentra en la base de datos  ");
+	
 	
 		    }
+
+
+
+
+
+	public static boolean menuAdmin(Scanner sc, boolean salir) {
+		int opcion;
+		while(!salir){
+			System.out.println("========== Bienvenid@  ==========");
+			System.out.println(" ");
+		    System.out.println("(Seleccione la opción que desee con ");
+			System.out.println("las teclas * 1 - 2 - 3 - 4 o 5* )");
+			System.out.println(" ");
+			System.out.println(" 1) Listar alquileres");
+			System.out.println(" 2) Editar alquileres");
+			System.out.println(" 3) Eliminar alquileres ");
+			System.out.println(" 4) Crear nuevo usuario ");
+			System.out.println(" 5) Cerrar sesión ");
+			System.out.println(" ");
+			System.out.println("======================================");
+			System.out.println(" ");
+			opcion = sc.nextInt();
+
+			switch(opcion){
+		    	case 1:
+		    	
+		    		//CONTECTAR CON BASE DE DATOS Y MOSTRAR POR PATNALLA LISTADO DE ALQUILERES
+		    		
+		    	break;
+			case 2:
+				
+					//CONTECTAR CON BASE DE DATOS PARA EDITAR ALQUILERES
+				
+		    	break;  
+			case 3:
+				
+				//CONTECTAR CON BASE DE DATOS PARA ELIMINAR ALQUILERES
+				
+				break;
+			case 4:
+		    	
+				//CONTECTAR CON BASE DE DATOS PARA CREAR USUARIO
+				
+				break;
+		 	case 5:                      	 
+		    	salir=true;
+		    	System.out.println("== SALIENDO DEL PROGRAMA ==");
+		    	System.out.println(" ");
+		    	System.out.println("Gracias por tu colaboración, que tengas un buen dia");
+		    	break;
+		 	default:
+		    	System.out.println("Solo números entre 1 y 5");
+		    	System.out.println(" ");  	 
+		  	 
+		  	}	 
+			}
+		return salir;
+	}
+	
+	
+	
+	 
+
+	public static boolean menuCliente(Connection con, Cliente c1, Scanner sc, boolean salir, String nombreIntroducido,
+			String nombreUsuario, String apellidosUsuario) throws SQLException {
+		int opcion;
+		//SQL que recoga los datos y poner el nombre del usuario 
+		  	
+		  	System.out.println("Bienvenido " + nombreUsuario + " " + apellidosUsuario);
+		  	System.out.println(" ");
+		  	
+			
+		  	 
+			while(!salir){
+			System.out.println("========== QUE DESEA HACER? ==========");
+			System.out.println(" ");
+		    System.out.println("(Seleccione la opción que desee con ");
+			System.out.println("las teclas * 1 - 2 o 3* )");
+			System.out.println(" ");
+			System.out.println(" 1) Mostrar información de su vehiculo");
+			System.out.println(" 2) Mostrar su información personal");
+			System.out.println(" 3) Salir de la aplicación ");
+			System.out.println(" ");
+			System.out.println("======================================");
+			System.out.println(" ");
+			opcion = sc.nextInt();
+			
+
+			switch(opcion){
+		    	case 1:
+		    	
+		    		//CONTECTAR CON BASE DE DATOS Y MOSTRAR POR PANTALLA INFORMACIÓN DEL VEHÍCULO DEL CLIENTE
+		    		
+		    	break;
+			case 2:
+				
+					//CONTECTAR CON BASE DE DATOS Y MOSTRAR POR PANTALLA INFORMACIÓN DEL CLIENTE 
+				
+				c1.buscartuInformacion(con);
+				
+		    	break;       	
+		 	case 3:                      	 
+		    	salir=true;
+		    	System.out.println("== SALIENDO DEL PROGRAMA ==");
+		    	System.out.println(" ");
+		    	System.out.println("Gracias por tu colaboración " +nombreIntroducido+ ", que tengas un buen dia");
+		    	System.exit(0);
+		    	break;
+		 	default:
+		    	System.out.println("Solo números entre 1 y 3");
+		    	System.out.println(" ");  	 
+		  	 
+		  	}	 
+			}
+		return salir;
+	}
 	}	 
 
 
